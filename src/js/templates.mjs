aad6450ac54e0bee68a4bb6c087e774fd7157f18
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-10-07 10:16:03 -0600

    added to

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -4,25 +4,25 @@ export function parkInfoTemplate(info) {
         <span>${info.designation}</span>
         <span>${info.states}</span>
       </p>`;
-  } 
-
+  }
+  
   export function mediaCardTemplate(info) {
     return `<div class="media-card">
       <a href="${info.link}">
       <img src="${info.image}" alt="${info.name}" class="media-card__img">
       <h3 class="media-card__title">${info.name}</h3>
-      </a> 
-     <p>${info.description}</p> 
-       </div>`; 
+      </a>
+     <p>${info.description}</p>
+       </div>`;
   }
   function getMailingAddress(addresses) {
     const mailing = addresses.find((address) => address.type === "Mailing");
-    return mailing; 
+    return mailing;
   }
   function getVoicePhone(numbers) {
     const voice = numbers.find((number) => number.type === "Voice");
     return voice.phoneNumber;
-  } 
+  }
   export function footerTemplate(info) {
     const mailing = getMailingAddress(info.addresses);
     const voice = getVoicePhone(info.contacts.phoneNumbers);
@@ -33,7 +33,7 @@ export function parkInfoTemplate(info) {
       <div><p>${mailing.line1}<p>
       <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
       <h4>Phone:</h4>
-      <p>${voice}</p>  
-    </section> 
-      `; 
+      <p>${voice}</p>
+    </section>
+      `;
   }  
