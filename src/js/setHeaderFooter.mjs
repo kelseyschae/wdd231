7f84501e2475eca41817f138361379d3fba5b291
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-12-04 10:55:22 -0700

    edits

--- src/js/setHeaderFooter.mjs
+++ src/js/setHeaderFooter.mjs
@@ -2,15 +2,18 @@ import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
 import enableNavigation from "./navigation.mjs";
 
 function setHeaderInfo(data) {
-  // insert data into disclaimer section
+  // disclaimer link
   const disclaimer = document.querySelector(".disclaimer > a");
   disclaimer.href = data.url;
   disclaimer.innerHTML = data.fullName;
-  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+
+  // document title
   document.querySelector("head > title").textContent = data.fullName;
-  // set the banner image
+
+  // hero image
   document.querySelector(".hero-banner > img").src = data.images[0].url;
-  // use the template function above to set the rest of the park specific info in the header
+
+  // hero content
   document.querySelector(".hero-banner__content").innerHTML =
     parkInfoTemplate(data);
 }
@@ -21,8 +24,13 @@ function setFooter(data) {
 }
 
 export default function setHeaderFooter(parkData) {
+  // Insert all HTML content
   setHeaderInfo(parkData);
   setFooter(parkData);
-  enableNavigation();
+
+  // Delay enabling navigation until DOM updates complete
+  requestAnimationFrame(() => {
+    enableNavigation();
+  });
 }
 
